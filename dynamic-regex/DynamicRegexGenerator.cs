4525b12848912a7dfc77f383fe9a781d8e6b0d5f
aleverifc <153004563+aleverifc@users.noreply.github.com>
2024-03-11 13:00:24 -0500

    change code for avoid notallow

--- dynamic-regex/DynamicRegexGenerator.cs
+++ dynamic-regex/DynamicRegexGenerator.cs
@@ -5,48 +5,38 @@ namespace dynamic_regex
 {
     public class DynamicRegexGenerator
     {
-        public string GenerateRegex(bool allowNumeric, bool allowAlpha, int minLength, int maxLength, string allowedSpecialChars = "", string notAllowedSpecialChars = "")
+        public string GenerateRegex(bool allowNumeric, bool allowAlpha, int minLength, int maxLength, string allowedSpecialChars = "")
         {
-            StringBuilder pattern = new();
+            StringBuilder pattern = new StringBuilder();
 
-            pattern.Append('^');
+            pattern.Append('^'); // Start of string
 
             if (allowNumeric && allowAlpha)
             {
-                pattern.Append("[a-zA-Z0-9");
+                pattern.Append("[a-zA-Z0-9"); // Include both alphabetic and numeric characters
             }
             else if (allowNumeric)
             {
-                pattern.Append("[0-9");
+                pattern.Append("[0-9"); // Include only numeric characters
             }
             else if (allowAlpha)
             {
-                pattern.Append("[a-zA-Z");
+                pattern.Append("[a-zA-Z"); // Include only alphabetic characters
             }
 
             if (!string.IsNullOrEmpty(allowedSpecialChars))
             {
                 foreach (char c in allowedSpecialChars)
                 {
-                    pattern.Append(Regex.Escape(c.ToString()));
+                    pattern.Append(Regex.Escape(c.ToString())); // Append allowed special characters, escaped
                 }
             }
 
-            pattern.Append(']');
+            pattern.Append(']'); // Close the character class
 
-            if (!string.IsNullOrEmpty(notAllowedSpecialChars))
-            {
-                pattern.Append("[^");
-                foreach (char c in notAllowedSpecialChars)
-                {
-                    pattern.Append(Regex.Escape(c.ToString()));
-                }
-                pattern.Append(']');
-            }
+            pattern.AppendFormat("{{{0},{1}}}", minLength, maxLength); // Specify the length range
 
-            pattern.AppendFormat("{{{0},{1}}}", minLength, maxLength);
-
-            pattern.Append('$');
+            pattern.Append('$'); // End of string
 
             return pattern.ToString();
         }
