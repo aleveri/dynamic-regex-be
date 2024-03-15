7437107aef9bbbc934e460009170acc3316079d6
aleverifc <153004563+aleverifc@users.noreply.github.com>
2024-03-15 14:08:58 -0500

    add daTA

--- dynamic-regex/Temporal.cs
+++ dynamic-regex/Temporal.cs
@@ -2,32 +2,33 @@
 
 namespace dynamic_regex
 {
-//   {
-//  "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//  "customerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//  "minLength": 0,
-//  "maxLength": 0,
-//  "allowedSpecialCharacters": "string",
-//  "prefix": "string",
-//  "suffix": "string",
-//  "numMaxLength": 0,
-//  "alphaMaxLength": 0,
-//  "manualOrder": true,
-//  "roundTrip": true,
-//  "nameValidationCategory": "string",
-//  "allowedDataTypes": "Numeric",
-//  "customerPOValidationCategories": [
+// {
+//  "id": "634677e7-fdb1-482d-88b2-6b99f2d782b7",
+//  "customerid": "9a177e08-266c-460b-9430-59f3ff276386",
+//  "minlength": 4,
+//  "maxlength": 15,
+//  "allowedspecialcharacters": "$%%%$",
+//  "prefix": "dwj",
+//  "suffix": "T4s",
+//  "nummaxlength": 10,
+//  "alphamaxlength": 8,
+//  "manualorder": false,
+//  "roundtrip": false,
+//  "namevalidationcategory": "LOMvQo",
+//  "alloweddatatypes": "numeric",
+//  "customerpovalidationcategories": [
 //    {
-//      "customerPoValidationId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//      "poCategoryId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+//      "customerpovalidationid": "8077cb57-e283-4d8e-9b88-5d7ae592bf83",
+//      "pocategoryid": "4a800b77-0d5f-4594-bb27-34aff213e435"
 //    }
 //  ],
-//  "customerPOValidationSourcesApp": [
+//  "customerpovalidationsourcesapp": [
 //    {
-//      "customerPoValidationId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//      "sourceAppName": "string",
+//      "customerpovalidationid": "42a9b896-79b5-4569-9295-ce44cba03512",
+//      "sourceappname": "epIdw5",
 //      "editable": true
 //    }
 //  ]
 //}
+
 }
